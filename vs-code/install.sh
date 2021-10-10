ba448ce9e7aad50885d62dde6ce4dc32ba56e825
Alexandre Felipe <alexandre.f@academico.ifg.edu.br>
2021-10-10 18:05:26 -0300

    add: symbolic link to vs-code

--- vs-code/install.sh
+++ vs-code/install.sh
@@ -2,7 +2,12 @@
 
 wget -c https://az764295.vo.msecnd.net/stable/ee8c7def80afc00dd6e593ef12f37756d8f504ea/code-stable-x64-1633631666.tar.gz -P /tmp/
 
+#Download icon
+wget -c https://i.ibb.co/wyqRgY0/vscodeicon.png -P /tmp/
+
 cd /opt && sudo tar -xvzf /tmp/code-stable-x64-1633631666.tar.gz
+
+cd /tmp && sudo mv vscodeicon.png /opt/VSCode-linux-x64/resources/app/resources/linux/
 
 mkdir /tmp/code-desktop-file && cd /tmp/code-desktop-file
 
@@ -12,7 +17,7 @@ Name=Visual Studio Code
 Type=Application
 Exec=/opt/VSCode-linux-x64/code
 Terminal=false
-Icon=/opt/VSCode-linux-x64/resources/app/resources/linux/code.png
+Icon=/opt/VSCode-linux-x64/resources/app/resources/linux/vscodeicon.png
 Comment=Free. Built on open source. Runs everywhere.
 NoDisplay=false
 Categories=Development;IDE;
@@ -20,6 +25,9 @@ Name[en]=Visual Studio Code
 Name[en_US]=Visual Studio Code
 EOT
 
+
 sudo desktop-file-install /tmp/code-desktop-file/visual-studio-code.desktop
+
+sudo ln -s /opt/VSCode-linux-x64/code /usr/bin/code 
 
 clear && echo -e "Visual Studio Code installed!"
--- vs-code/remove.sh
+++ vs-code/remove.sh
@@ -2,3 +2,4 @@
 
 sudo rm /usr/share/applications/visual-studio-code.desktop
 sudo rm -rf /opt/VSCode-linux-x64
+sudo rm /usr/bin/code
