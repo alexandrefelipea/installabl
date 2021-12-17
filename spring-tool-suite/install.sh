165f2eda28db5861c0e840d9e2fc31c6616dca2d
Alexandre Felipe <alexandre.f@academico.ifg.edu.br>
2021-12-17 20:35:01 -0300

    upgrade: spring tool suite

--- spring-tool-suite/install.sh
+++ spring-tool-suite/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -c https://download.springsource.com/release/STS4/4.12.0.RELEASE/dist/e4.21/spring-tool-suite-4-4.12.0.RELEASE-e4.21.0-linux.gtk.x86_64.tar.gz -P /tmp/
+wget -c https://download.springsource.com/release/STS4/4.13.0.RELEASE/dist/e4.22/spring-tool-suite-4-4.13.0.RELEASE-e4.22.0-linux.gtk.x86_64.tar.gz -P /tmp/        
 
-cd /opt && sudo tar -xvzf /tmp/spring-tool-suite-4-4.12.0.RELEASE-e4.21.0-linux.gtk.x86_64.tar.gz
+cd /opt && sudo tar -xvzf /tmp/spring-tool-suite-4-4.13.0.RELEASE-e4.22.0-linux.gtk.x86_64.tar.gz
 
 mkdir /tmp/STSFile && cd /tmp/STSFile/
 
@@ -10,10 +10,10 @@ sudo cat >'spring-tool-suite.desktop' <<EOT
 [Desktop Entry]
 Name=Spring Tool Suite
 Type=Application
-Exec=/opt/sts-4.12.0.RELEASE/SpringToolSuite4
+Exec=/opt/sts-4.13.0.RELEASE/SpringToolSuite4
 Terminal=false
-Icon=/opt/sts-4.12.0.RELEASE/icon.xpm
-Comment=The all-new Spring Tool Suite 4.12
+Icon=/opt/sts-4.13.0.RELEASE/icon.xpm
+Comment=The all-new Spring Tool Suite
 NoDisplay=false
 Categories=Development;IDE;
 Name[en]=Spring Tool Suite
@@ -22,4 +22,4 @@ EOT
 
 sudo desktop-file-install /tmp/STSFile/spring-tool-suite.desktop
 
-clear && echo -e "Spring Tool Suite 4.12 installed!"
+clear && echo -e "Spring Tool Suite installed!"
--- spring-tool-suite/remove.sh
+++ spring-tool-suite/remove.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo rm /usr/share/applications/spring-tool-suite.desktop
-sudo rm -rf /opt/sts-4.12.0.RELEASE
+sudo rm -rf /opt/sts-4.13.0.RELEASE
